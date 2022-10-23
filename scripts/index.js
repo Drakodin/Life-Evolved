0f394d022bc5738d7c7c75332d42cc59c3a2b402
Drakodin <talia.yl.tian@gmail.com>
2022-10-23 16:22:57 -0500

    Slow default loop to reduce high load lag.

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,7 @@ const generateEnvironment = (params = undefined) => {
 /**
  * Render-level loop runner using Environment.next()
  */
-const runLoop = (speed = 5) => {
+const runLoop = (speed = 2) => {
     let info = env.info
     if (info.executionId) {
         return
